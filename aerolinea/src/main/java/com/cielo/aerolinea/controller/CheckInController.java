350ae8a081b5133010a118a55f9ec5d89929fe43
katz1124 <katz1124@hotmail.com>
2022-02-18 16:41:28 -0600

    previo a probar con thymeleaf
    libreria th agregada
    ya funciona primer servicio

--- aerolinea/src/main/java/com/cielo/aerolinea/controller/CheckInController.java
+++ aerolinea/src/main/java/com/cielo/aerolinea/controller/CheckInController.java
@@ -7,14 +7,11 @@ import com.cielo.aerolinea.service.SeatSelectionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
-@RestController
+@Controller
 @RequestMapping
 public class CheckInController {
 
@@ -23,13 +20,42 @@ public class CheckInController {
     @Autowired
     SeatSelectionService seatSelectionService;
 
+    @GetMapping("/")
+    public String home()
+    {
+        return "views/checkin/index";
+    }
 
     @GetMapping(value = "/checkin", params = {"name","reservationCode"})
     public Reservation checkin(@RequestParam String name , @RequestParam String reservationCode) {
 
         return availableForCheckinService.validateWithCode(name,reservationCode);
     }
+    /*
+    @GetMapping(value = "/checkin2/{name}/{code}")
+    public String checkin2(@PathVariable("name")String name,@PathVariable("code")String code) {
+        Reservation reservation=availableForCheckinService.validateWithCode(name,code);
+        if(reservation==null){
+            return "views/checkin/checkin2";
+        }
+        else{
+            return "views/planeroom/planeroom";
+        }
 
+    }
+    */
+    @ResponseBody
+    @GetMapping(value = "/checkin2/{name}/{code}")
+    public Reservation checkin2(@PathVariable("name")String name,@PathVariable("code")String code) {
+        Reservation reservation=availableForCheckinService.validateWithCode(name,code);
+        if(reservation==null){
+            return null;
+        }
+        else{
+            return reservation;
+        }
+
+    }
     @GetMapping(value = "/checkin/boardingpass")
     public BoardingPass boardingpass() {
         return seatSelectionService.generateBoardingPass(1,2,"B");
